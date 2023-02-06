f08103a2da433720a6120e92464f06656bf8bc0e
Aleksandra Korczak <alekorc@dfds.com>
2023-02-06 11:28:56 +0100

    Use default certificate

--- cloudfront.tf
+++ cloudfront.tf
@@ -35,6 +35,10 @@ module "cloudfront" {
   target_origin_id       = module.crl_bucket[0].bucket_name
   viewer_protocol_policy = "redirect-to-https"
 
+  viewer_certificate = {
+    cloudfront_default_certificate = true
+  }
+
   tags = var.cloudfront_tags
 }
 
