5b1330c8665f9e8642ab6b2f0342e7a14f1ea0e6
Aleksandra Korczak <alekorc@dfds.com>
2023-02-28 14:27:56 +0100

    Add cloudfront logging

---
+++ s3-bucket-cloudfront-logs.tf
@@ -0,0 +1,16 @@
+module "cloudfront_logs_bucket" {
+  count  = var.enable_crl ? 1 : 0
+  source = "git::https://github.com/dfds/aws-modules-s3.git?ref=v1.1.0"
+  providers = {
+    aws = aws.crl
+  }
+
+  bucket_name                     = var.cloudfront_logs_bucket
+  bucket_versioning_configuration = "Enabled"
+  object_ownership                = "BucketOwnerPreferred"
+  create_policy                   = false
+
+  kms_key_arn = aws_kms_key.this[0].arn
+
+  logging_bucket_name = module.s3_logs_bucket[0].bucket_name
+}
